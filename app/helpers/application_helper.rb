515e7d5b267585d9fadeb8bc615ad7656cb28507
Jonathan Bennett <jonathan@jbennett.me>
2019-06-06 03:13:34 -0400

    scaffolding homepage

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+
+  def random_header_url
+    images = Dir.new(Rails.root.to_s + "/app/assets/images/headers").select{ |f| f.downcase.match(/\.jpg|\.jpeg|\.png/) }
+    image_url("headers/" + images.sample)
+  end
 end
